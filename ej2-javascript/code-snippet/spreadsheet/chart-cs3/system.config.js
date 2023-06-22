16934e5d4547bbaf6816149554eb4d3c2dda417c
SharanSF4079 <sharan.muthuraja@syncfusion.com>
2023-06-22 19:40:56 +0530

    documentation(EJ2-822839): changed version number

--- ej2-javascript/code-snippet/spreadsheet/chart-cs3/system.config.js
+++ ej2-javascript/code-snippet/spreadsheet/chart-cs3/system.config.js
@@ -10,7 +10,7 @@ System.config({
         }
     },
     paths: {
-        "syncfusion:": "https://cdn.syncfusion.com/ej2/20.3.56/"
+        "syncfusion:": "https://cdn.syncfusion.com/ej2/22.1.34/"
     },
     map: {
         main: "index.ts",
