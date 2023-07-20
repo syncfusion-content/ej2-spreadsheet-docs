e5f8a30a62c33848ba4896bb0bf12bd8c90513b7
sangetha-priya-m <sangeetha.murugan@syncfusion.com>
2023-07-20 19:04:02 +0530

    Documentation(378210): Localization and globalization related changes committed

--- ej2-javascript/code-snippet/spreadsheet/global/rtl-cs1/index.ts
+++ ej2-javascript/code-snippet/spreadsheet/global/rtl-cs1/index.ts
@@ -1,62 +1,29 @@
-
-
-import { Spreadsheet, SheetModel } from '@syncfusion/ej2-spreadsheet';
+import { Spreadsheet, ColumnModel } from '@syncfusion/ej2-spreadsheet';
 import { defaultData } from './datasource.ts';
 import { L10n, enableRipple } from '@syncfusion/ej2-base';
-
+import * as localeText from './locale-text.json';
 enableRipple(true);
 
-L10n.load({
-    'ar-AE': {
-        'spreadsheet': {
-            "File": "ملف",
-            "Home": "هم",
-            "Insert": "إدراج",
-            "Formulas": "الصيغ",
-            "View": "معاينة",
-            "Data": "البيانات",
-            "Cut": "قطع",
-            "Copy": "نسخ",
-            "Paste": "معجون",
-            "PasteSpecial": "لصق خاص",
-            "All": "جميع",
-            "Values": "القيم",
-            "Formats": "شكل",
-            "Font": "الخط",
-            "FontSize": "حجم الخط",
-            "Bold": "جريء",
-            "Italic": "مائل",
-            "Underline": "أكد",
-            "Strikethrough": "يتوسطه",
-            "TextColor": "لون الخط",
-            "FillColor": "لون التعبئة",
-            "HorizontalAlignment": "المحاذاة الأفقية",
-            "AlignLeft": "محاذاة إلى اليسار",
-            "AlignCenter": "مركز",
-            "AlignRight": "محاذاة إلى اليمين",
-            "VerticalAlignment": "محاذاة عمودية",
-            "AlignTop": "محاذاة أعلى",
-            "AlignMiddle": "محاذاة الأوسط",
-            "AlignBottom": "أسفل محاذاة",
-            "InsertFunction": "إدراج وظيفة",
-            "Delete": "حذف",
-            "Rename": "إعادة تسمية",
-            "Hide": "إخفاء",
-            "Unhide": "ظهار"
-        }
-    }
-});
+L10n.load({ 'ar-AE': localeText });
 
-let columns: ColumnModel[] = [{ width: 100 }, { width: 130 },{ width: 96},
-    { width: 130 }, { width: 130 },{ width: 96},
-    { width: 100 }, { width: 100 },{ width: 110}, { width: 100 }, { width: 130 },{ width: 150}]
+let columns: ColumnModel[] = [
+  { width: 100 },
+  { width: 130 },
+  { width: 96 },
+  { width: 130 },
+  { width: 130 },
+  { width: 96 },
+  { width: 100 },
+  { width: 100 },
+  { width: 110 },
+  { width: 100 },
+  { width: 130 },
+  { width: 150 },
+];
 
 let spreadsheet: Spreadsheet = new Spreadsheet({
-        locale: 'ar-AE',
-        enableRtl: true,
-        sheets: [{ ranges: [{ dataSource: defaultData }], columns: columns
-    }]
+  locale: 'ar-AE',
+  enableRtl: true,
+  sheets: [{ ranges: [{ dataSource: defaultData }], columns: columns }],
 });
 spreadsheet.appendTo('#spreadsheet');
-
-
