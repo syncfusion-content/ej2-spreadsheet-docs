1d281cebb06b3ae99206f5d9fc74eb5fb9da6c92
Babu <babu.sf3928@gmail.com>
2023-07-31 16:35:41 +0530

    Addressed the review corrections.

--- ej2-javascript/code-snippet/spreadsheet/data-binding-cs2/index.js
+++ ej2-javascript/code-snippet/spreadsheet/data-binding-cs2/index.js
@@ -4,7 +4,7 @@ var query = new ej.data.Query().select([
 
     //Initialize DataManager.
     var data = new ej.data.DataManager({
-        url:  'https://ej2services.syncfusion.com/production/web-services/api/Orders',
+        url:  'https://services.syncfusion.com/js/production/api/Orders',
         crossDomain: true
     });
 
