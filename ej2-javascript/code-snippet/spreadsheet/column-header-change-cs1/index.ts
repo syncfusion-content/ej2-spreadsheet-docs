eba56d674471dbd45d937973372776e656bd53c7
Babu <babu.sf3928@gmail.com>
2023-08-03 21:29:13 +0530

    Addressed the review corrections.

--- ej2-javascript/code-snippet/spreadsheet/column-header-change-cs1/index.ts
+++ ej2-javascript/code-snippet/spreadsheet/column-header-change-cs1/index.ts
@@ -1,21 +1,18 @@
 import { Spreadsheet, CellRenderEventArgs } from '@syncfusion/ej2-spreadsheet';
 import { enableRipple } from '@syncfusion/ej2-base';
-
 enableRipple(true);
 
 let spreadsheet: Spreadsheet = new Spreadsheet({
-    beforeCellRender: (args: CellRenderEventArgs) => {
-      if (
-        args.colIndex >= 0 &&
-        args.colIndex <= 10 &&
-        args.element.classList.contains('e-header-cell')
-      ) {
-        let text: string = 'custom header ' + args.colIndex.toString();
-        args.element.innerText = text;
-      }
+  beforeCellRender: (args: CellRenderEventArgs) => {
+    if (
+      args.colIndex >= 0 &&
+      args.colIndex <= 10 &&
+      args.element.classList.contains('e-header-cell')
+    ) {
+      let text: string = 'custom header ' + args.colIndex.toString();
+      args.element.innerText = text;
     }
-  });
+  }
+});
 
 spreadsheet.appendTo('#spreadsheet');
-
-
