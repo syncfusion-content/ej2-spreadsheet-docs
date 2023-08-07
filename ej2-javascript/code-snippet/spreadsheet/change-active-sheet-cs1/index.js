dfaa58db2797f731cd4ebb2e7157ffaea4b4c647
gobalakrishnans <gobalakrishnans@syncfusion.com>
2023-08-07 15:57:45 +0530

    Update index.js

--- ej2-javascript/code-snippet/spreadsheet/change-active-sheet-cs1/index.js
+++ ej2-javascript/code-snippet/spreadsheet/change-active-sheet-cs1/index.js
@@ -1,6 +1,6 @@
 ej.base.enableRipple(true);
 
-var spreadsheet = new ej.spreadsheet.Spreadsheet({
+let spreadsheet = new ej.spreadsheet.Spreadsheet({
     openUrl: 'https://services.syncfusion.com/js/production/api/spreadsheet/open',
     saveUrl: 'https://services.syncfusion.com/js/production/api/spreadsheet/save',
     openComplete: function() {
