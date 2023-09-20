946afe6fb549a96db3b4186943be55911c3bb77b
sangeethapriya <sangeetha.murugan@syncfusion.com>
2023-09-20 20:40:49 +0530

    Documentation(378723): How to import excel file using file uploader in spreadsheet

--- ej2-javascript/code-snippet/spreadsheet/import-using-uploader/index.ts
+++ ej2-javascript/code-snippet/spreadsheet/import-using-uploader/index.ts
@@ -8,16 +8,18 @@ let spreadsheet: Spreadsheet = new Spreadsheet({
 spreadsheet.appendTo('#spreadsheet');
 
 // Initialize the uploader component
-let uploadObj: Uploader = new Uploader({
+let uploader: Uploader = new Uploader({
   asyncSettings: {
-    saveUrl: 'https://ej2.syncfusion.com/services/api/uploadbox/Save',
-    removeUrl: 'https://ej2.syncfusion.com/services/api/uploadbox/Remove',
+    saveUrl:
+      'https://services.syncfusion.com/js/production/api/FileUploader/Save',
+    removeUrl:
+      'https://services.syncfusion.com/js/production/api/FileUploader/Remove',
   },
   success: onSuccess,
-  allowedExtensions: '.xlsx, .xls, .csv',
+  allowedExtensions: '.csv, .xls, .xlsx',
 });
-uploadObj.appendTo('#fileupload');
+uploader.appendTo('#fileupload');
 
 function onSuccess(args): void {
-  if (args.operation == 'upload') spreadsheet.open({ file: args.file.rawFile }); // to open the loaded excel file in spreadsheet
+  if (args.operation == 'upload') spreadsheet.open({ file: args.file.rawFile });
 }
