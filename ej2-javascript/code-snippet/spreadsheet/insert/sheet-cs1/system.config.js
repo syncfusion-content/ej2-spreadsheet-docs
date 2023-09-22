debe146363a607bea5b8c2c50418b9007461f2e8
Babu <babu.sf3928@gmail.com>
2023-09-22 13:58:48 +0530

    documentation(EJ2-849272):Added the notifications package in the React documentation.

--- ej2-javascript/code-snippet/spreadsheet/insert/sheet-cs1/system.config.js
+++ ej2-javascript/code-snippet/spreadsheet/insert/sheet-cs1/system.config.js
@@ -10,7 +10,7 @@ System.config({
         }
     },
     paths: {
-        "syncfusion:": "https://cdn.syncfusion.com/ej2/20.3.56/"
+        "syncfusion:": "https://cdn.syncfusion.com/ej2/23.1.36/"
     },
     map: {
         main: "index.ts",
@@ -23,6 +23,7 @@ System.config({
         "@syncfusion/ej2-splitbuttons": "syncfusion:ej2-splitbuttons/dist/ej2-splitbuttons.umd.min.js",
         "@syncfusion/ej2-lists": "syncfusion:ej2-lists/dist/ej2-lists.umd.min.js",
         "@syncfusion/ej2-navigations": "syncfusion:ej2-navigations/dist/ej2-navigations.umd.min.js",
+        "@syncfusion/ej2-notifications": "syncfusion:ej2-notifications/dist/ej2-notifications.umd.min.js",
         "@syncfusion/ej2-popups": "syncfusion:ej2-popups/dist/ej2-popups.umd.min.js",
         "@syncfusion/ej2-dropdowns": "syncfusion:ej2-dropdowns/dist/ej2-dropdowns.umd.min.js",
         "@syncfusion/ej2-calendars": "syncfusion:ej2-calendars/dist/ej2-calendars.umd.min.js",
