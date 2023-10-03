bf62a8b2d47d46c6d03dd16d9581ea4dae2105d5
sangeethapriya <sangeetha.murugan@syncfusion.com>
2023-10-03 21:59:14 +0530

    Documentation(378723): How to find presently opened context menu in spreadsheet

--- ej2-javascript/code-snippet/spreadsheet/find-target-context-menu/index.js
+++ ej2-javascript/code-snippet/spreadsheet/find-target-context-menu/index.js
@@ -3,13 +3,13 @@ ej.base.enableRipple(true);
 let spreadsheet = new ej.spreadsheet.Spreadsheet({
   contextMenuBeforeOpen: function (args) {
     if (ejs.base.closest(args.event.target, '.e-sheet-content')) {
-      console.log('Cell contextMenu');
+      console.log('Cell Context Menu');
     } else if (ejs.base.closest(args.event.target, '.e-colhdr-table')) {
-      console.log('Column header contextMenu');
+      console.log('Column Header Context Menu');
     } else if (ejs.base.closest(args.event.target, '.e-rowhdr-table')) {
-      console.log('Row header contextMenu');
+      console.log('Row Header Context Menu');
     } else if (ejs.base.closest(args.event.target, '.e-toolbar-item')) {
-      console.log('Footer contextMenu');
+      console.log('Footer Context Menu');
     }
   },
 });
