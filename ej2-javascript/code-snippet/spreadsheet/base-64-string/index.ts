b851b3a74a7908d7cb2506c49c28cf4312644a9d
Vasanth-3519 <vasanth.ravi@syncfusion.com>
2023-11-22 02:23:56 +0530

    Added Load and Save the spreadsheet data as Base64 string in client side content in how to section.

---
+++ ej2-javascript/code-snippet/spreadsheet/base-64-string/index.ts
@@ -0,0 +1,62 @@
+
+import { Spreadsheet, BeforeSaveEventArgs, SaveCompleteEventArgs } from '@syncfusion/ej2-spreadsheet';
+import { data } from './datasource.ts';
+
+let blob: Blob; let base64String: any;
+
+//Initialize the SpreadSheet control
+let spreadsheet: Spreadsheet = new Spreadsheet({
+
+  sheets: [{
+    name: 'Car Sales Report',
+    ranges: [{ dataSource: data }],
+    columns: [
+      { width: 180 }, { width: 130 }, { width: 130 }, { width: 180 },
+      { width: 130 }, { width: 120 }
+    ]
+  }],
+
+  created: (): void => {
+    //Applies cell and number formatting to specified range of the active sheet
+    spreadsheet.cellFormat({ fontWeight: 'bold', textAlign: 'center', verticalAlign: 'middle' }, 'A1:F1');
+  },
+
+  beforeSave: (args: BeforeSaveEventArgs): void => {
+    args.needBlobData = true; //To trigger saveComplete event
+    args.isFullPost = false; // To get blob data of Spreadsheet
+  },
+
+  saveComplete: (args: SaveCompleteEventArgs): void => {
+    // Assign blobdata to a variable.
+    blob = args.blobData;
+    // Convert blob data to base64 string.
+    let reader: FileReader = new FileReader();
+    reader.readAsDataURL(blob);
+    reader.onloadend = function () {
+      base64String = reader.result;
+    };
+  }
+
+});
+
+spreadsheet.appendTo('#spreadsheet');
+
+document.getElementById("import").onclick = () => {
+  // Open the file based on saved base64 string.
+  fetch(base64String)
+    .then((response) => response.blob())
+    .then((fileBlob) => {
+      var file = new File([fileBlob], 'Sample.xlsx'); // to open the blob file
+      spreadsheet.open({ file: file });
+    });
+}
+
+document.getElementById("export").onclick = () => {
+  spreadsheet.save({
+    url: 'https://services.syncfusion.com/js/production/api/spreadsheet/save',
+    fileName: 'Worksheet',
+    saveType: 'Xlsx',
+  }); // Specifies the save URL, filename, file type need to be saved.
+  // Logs base64 string into the console.
+  console.log('Base64 String - ', base64String);
+}
