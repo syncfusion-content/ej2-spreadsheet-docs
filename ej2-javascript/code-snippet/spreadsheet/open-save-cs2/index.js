d29060cab19d166d5e487ffa660126afee62b570
Babu <babu.sf3928@gmail.com>
2023-12-01 16:09:41 +0530

    bug(EJ-858413):Changed the hosted excel file link with the new link.

--- ej2-javascript/code-snippet/spreadsheet/open-save-cs2/index.js
+++ ej2-javascript/code-snippet/spreadsheet/open-save-cs2/index.js
@@ -3,7 +3,7 @@
         openUrl: 'https://services.syncfusion.com/js/production/api/spreadsheet/open',
         saveUrl: 'https://services.syncfusion.com/js/production/api/spreadsheet/save',
         created: function () {
-            fetch("https://js.syncfusion.com/demos/ejservices/data/Spreadsheet/LargeData.xlsx") // fetch the remote url
+            fetch("https://cdn.syncfusion.com/scripts/spreadsheet/Sample.xlsx") // fetch the remote url
                 .then((response) => {
                     response.blob().then((fileBlob) => { // convert the excel file to blob
                     var file = new File([fileBlob], "Sample.xlsx"); //convert the blob into file
